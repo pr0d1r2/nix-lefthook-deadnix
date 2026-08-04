373b16a1ae49271a7d3c45d13b7fa21dab01e7bd
Hallucinogen Codex Agent <agent-codex@hallucinogen.local>
2026-08-04 22:43:34 +0000

    Fix flake guardrail configuration

--- flake.nix
+++ flake.nix
@@ -10,7 +10,10 @@
     nixpkgs-lock.url = "github:pr0d1r2/nixpkgs-lock";
     nixpkgs.follows = "nixpkgs-lock/nixpkgs";
 
-    set-and-setting.url = "github:pr0d1r2/set-and-setting";
+    set-and-setting = {
+      url = "github:pr0d1r2/set-and-setting";
+      inputs.nixpkgs-lock.follows = "nixpkgs-lock";
+    };
   };
 
   outputs =
@@ -20,7 +23,8 @@
       set-and-setting,
       ...
     }:
-    let
+    set-and-setting.lib.mkConsumerFlake {
+      inherit self nixpkgs set-and-setting;
       fragments = [
         "base"
         "nix"
@@ -29,59 +33,82 @@
         "markdown"
         "yaml"
       ];
-      consumer = set-and-setting.lib.mkConsumerFlake {
-        inherit self nixpkgs set-and-setting;
-        inherit fragments;
-        extraPackages = pkgs: {
-          default = pkgs.writeShellApplication {
-            name = "lefthook-deadnix";
-            runtimeInputs = [ pkgs.deadnix ];
-            text = builtins.readFile ./lefthook-deadnix.sh;
-          };
+      extraPackages = pkgs: {
+        default = pkgs.writeShellApplication {
+          name = "lefthook-deadnix";
+          runtimeInputs = [ pkgs.deadnix ];
+          text = builtins.readFile ./lefthook-deadnix.sh;
         };
-        src = ./.;
       };
-      confirmFor =
-        pkgs:
-        let
-          materialization = set-and-setting.lib.materializationFor {
-            inherit pkgs fragments;
-          };
-          confirm = pkgs.writeShellApplication {
-            name = "confirm";
-            runtimeInputs = [
-              pkgs.coreutils
-              pkgs.diffutils
-              pkgs.findutils
-              pkgs.gawk
-              pkgs.git
-              pkgs.gnugrep
-            ]
-            ++ materialization.packages;
-            runtimeEnv = {
-              FRAGMENTS_DIR = "${set-and-setting}/setting/integrations/lefthook";
-              ASSEMBLE_SCRIPT = "${set-and-setting}/setting/lib/assemble-lefthook.sh";
-              DETECT_SCRIPT = "${set-and-setting}/setting/lib/detect-fragments.sh";
-              SETTING_SRC = "${self.packages.${pkgs.stdenv.hostPlatform.system}.setting}";
-              CONFIRM_SCRIPT = "${set-and-setting}/lib/confirm.sh";
-              CONFIRM_REV = set-and-setting.rev or "unknown";
-            };
-            text = builtins.readFile ./confirm.sh;
-          };
-        in
-        {
-          type = "app";
-          program = "${confirm}/bin/confirm";
-        };
-    in
-    consumer
+      src = ./.;
+    }
     // {
-      apps = nixpkgs.lib.mapAttrs (
-        system: apps:
-        apps
-        // {
-          confirm = confirmFor nixpkgs.legacyPackages.${system};
-        }
-      ) consumer.apps;
+      apps =
+        nixpkgs.lib.mapAttrs
+          (
+            system: apps:
+            apps
+            // {
+              confirm =
+                let
+                  pkgs = nixpkgs.legacyPackages.${system};
+                  materialization = set-and-setting.lib.materializationFor {
+                    inherit pkgs;
+                    fragments = [
+                      "base"
+                      "nix"
+                      "shell"
+                      "ascii"
+                      "markdown"
+                      "yaml"
+                    ];
+                  };
+                  confirm = pkgs.writeShellApplication {
+                    name = "confirm";
+                    runtimeInputs = [
+                      pkgs.coreutils
+                      pkgs.diffutils
+                      pkgs.findutils
+                      pkgs.gawk
+                      pkgs.git
+                      pkgs.gnugrep
+                    ]
+                    ++ materialization.packages;
+                    runtimeEnv = {
+                      FRAGMENTS_DIR = "${set-and-setting}/setting/integrations/lefthook";
+                      ASSEMBLE_SCRIPT = "${set-and-setting}/setting/lib/assemble-lefthook.sh";
+                      DETECT_SCRIPT = "${set-and-setting}/setting/lib/detect-fragments.sh";
+                      SETTING_SRC = "${self.packages.${system}.setting}";
+                      CONFIRM_SCRIPT = "${set-and-setting}/lib/confirm.sh";
+                      CONFIRM_REV = set-and-setting.rev or "unknown";
+                    };
+                    text = builtins.readFile ./confirm.sh;
+                  };
+                in
+                {
+                  type = "app";
+                  program = "${confirm}/bin/confirm";
+                };
+            }
+          )
+          (set-and-setting.lib.mkConsumerFlake {
+            inherit self nixpkgs set-and-setting;
+            fragments = [
+              "base"
+              "nix"
+              "shell"
+              "ascii"
+              "markdown"
+              "yaml"
+            ];
+            extraPackages = pkgs: {
+              default = pkgs.writeShellApplication {
+                name = "lefthook-deadnix";
+                runtimeInputs = [ pkgs.deadnix ];
+                text = builtins.readFile ./lefthook-deadnix.sh;
+              };
+            };
+            src = ./.;
+          }).apps;
     };
 }
